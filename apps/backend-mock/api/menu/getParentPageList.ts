4bf081d3d71c78cc9debe9b827bc4a084658662b
Yxl <4999844677@qq.com>
2024-09-01 21:21:04 +0800

    feat: 调整注释

--- apps/backend-mock/api/menu/getParentPageList.ts
+++ apps/backend-mock/api/menu/getParentPageList.ts
@@ -7,11 +7,11 @@ export default eventHandler((event) => {
   }
 
   // const username = Buffer.from(token, 'base64').toString('utf8');
-  const query = getQuery(event);
+  // const query = getQuery(event);
 
-  if (query.NameOrCode !== undefined) {
-    MOCK_ParentMenu_List.filter((a) => a.Name.includes(query.NameOrCode));
-  }
+  // if (query.NameOrCode !== undefined) {
+  //   MOCK_ParentMenu_List.filter((a) => a.Name.includes(query.NameOrCode));
+  // }
 
   // const menus =
   //   MOCK_MENUS.find((item) => item.username === username)?.menus ?? [];
